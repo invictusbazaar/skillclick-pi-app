b96616825970aaa73575f22a2450102f56cfd277
invictusbazaar <invictusbazaar@gmail.com>
2025-11-28 14:04:19 +0100

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
           }}
         />
 
-
         <div className="flex flex-col min-h-screen">
           <div className="flex-grow">{children}</div>
           <Footer />
